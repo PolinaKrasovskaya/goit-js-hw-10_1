ccdee57dac341d27d67993242dcd69f4e175e9e4
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2021-11-27 05:08:56 +0200

    ready

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+import Notiflix from "notiflix";
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+
+export function fetchCountries (name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        if(!response.ok) {
+            throw Notiflix.Notify.failure("Oops, there is no country with that name")
+        }
+        return response.json();
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './css/styles.css';
-import { debounce } from 'lodash/debounce';
+import debounce from 'lodash/debounce';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.2.min.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -9,30 +10,51 @@ const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+
+function createCountry (country){
+    const markup = country.map(({ flags, name }) => {
+        return `<p><img src="${flags.svg}" alt="flag" width="30px"/>${name.official}</p>`
+    }).join('');
+
+    countryInfo.innerHTML = markup;
+};
+
+
+function createMarkup(data) {
+    const country = data.map(({ name, flags, capital, population, languages }) => {
+        return `
+        <h1><img src="${flags.svg}" class="country" alt="flag" width="30px"/>${name.official}</h1>
+        <ul>
+            <li>Capital:<span>${capital}</span></li>
+            <li>Population:<span>${population}</span></li>
+            <li>Languages:<span>${Object.values(languages).join(', ')}</span></li>
+        </ul>`
+    }).join('');
+
+    countryList.innerHTML = country;
+    countryInfo.innerHTML = ''
+};
+
+
 searchBox.addEventListener('input', debounce(() => {
-    const name = searchBox.value;
+    const name = searchBox.value.trim();
     fetchCountries(name).then(showCountry).catch(showError);
 }, DEBOUNCE_DELAY));
 
+
 function showError (error) {
     console.log(error);
-    countryInfo.innerHTML = 'Ойойой, что-то пошло не так, попробуй еще раз!';
 }
 
-function showCountry ({
-    name, 
-    capital, 
-    population, 
-    flags, 
-    languages,
-}) 
 
-function fetchCountries (name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
-    .then(response => {
-        if(!response.ok) {
-            return Promise.reject();
-        }
-        return response.json();
-    });
-}
+function showCountry(country) {
+    if (country.status === 404) {
+        return Notiflix.Notify.failure("Oops, there is no country with that name");
+    } else if (country.length > 10) {
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (country.length >= 2 && country.length <= 10) {
+        return createCountry(country)
+    } else {
+        return createMarkup(country)
+    }
+};
